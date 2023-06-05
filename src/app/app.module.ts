c053e021b932d8d634f3536ac69dbed4a4e028b8
Hoangldph21518 <Hoangldph21518@fpt.edu.vn>
2023-06-05 22:58:39 +0700

    feat: create ui Movies Pages refs: SP4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BaseLayoutClientComponent } from './layout/base-layout-client/base-layout-client.component';
 import { BaseLayoutAdminComponent } from './layout/base-layout-admin/base-layout-admin.component';
 import { BaseLayoutAuthComponent } from './layout/base-layout-auth/base-layout-auth.component';
+import { MoviesPageComponent } from './pages/movies-page/movies-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BaseLayoutClientComponent,
     BaseLayoutAdminComponent,
-    BaseLayoutAuthComponent
+    BaseLayoutAuthComponent,
+    MoviesPageComponent
   ],
   imports: [
     BrowserModule,
